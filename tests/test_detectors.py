d9fe8f72a454b70255127e631616289592464a01
idle sign <idlesign@yandex.ru>
2017-10-21 16:35:28 +0700

    Tests updated.

--- tests/test_detectors.py
+++ tests/test_detectors.py
@@ -13,11 +13,14 @@ def test_get_detector():
         get_detector('bogus')
 
 
-def test_file_detector():
+def test_file_detector(tmpdir):
     detector = get_detector('file')
 
     assert detector is File
 
-    env = get_environment(detectors_opts={'file': {'source': 'myenvironment'}})
+    path = tmpdir.mkdir('envboxtmp').join('myenvironment')
+    path.write('testing')
+
+    env = get_environment(detectors_opts={'file': {'source': '%s' % path}})
 
     assert env.is_testing
